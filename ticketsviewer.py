27ceb6de9c89b459e2c15d1ce7b8a8a7b89281af
Kaleab <t-kalemu@expediagroup.com>
2021-07-31 23:09:53 -0500

    Improved efficieny

--- ticketsviewer.py
+++ ticketsviewer.py
@@ -1,8 +1,13 @@
+#Name: Kaleab Alemu
+#Zendesk Coding Challenge
+#Date: 07/31/2021
+
 import requests
+import math
 import time
+
 # Set the request parameters
 count = 0
-urls = []
 user = 'kaleabunited@gmail.com' +'/token'
 pwd = 'lwpUCXiRmglWwug5EBLrpcM41SCza0awetv5AJmb'
 print("\nWelcome to our advanced Ticket Viewer")
@@ -19,51 +24,19 @@ def init():
         print('Status:', response.status_code, 'Problem with the request. Exiting....')
         exit()
     
-    # Do the HTTP get request to get number of tickets
-    count = response.json()['count']
-    
-    url = 'https://zccsenior.zendesk.com/api/v2/tickets.json?page[size]=25'
-    urls.append(url)
-
-    #Fetch urls for all pages
-    while url:
-        response = requests.get(url, auth=(user, pwd))
-        data = response.json()
-        if data['meta']['has_more']:
-            url = data['links']['next']
-            urls.append(url)
-        else:
-            url = None
-
-def fetchTickets(page):
-    #fecth all all tickets based on url from urls[]
-    
-    print("\nPage " + str(page) + " of " + str(len(urls)) + "\n")
     # Do the HTTP get request
-    start = time.time()
-    response = requests.get(urls[page-1], auth=(user, pwd))
-    end = time.time()
-    print("\nAPI response time %.2f" % ((end-start)*1000) + " microseconds.\n")
-
-    # Check for HTTP codes other than 200
-    if response.status_code != 200:
-        print('Status:', response.status_code, 'Problem with the request. Exiting....')
-        exit()
-
-    # Decode the JSON response into a dictionary and use the data
-    data = response.json()
-    return data
+    count = response.json()['count']
 
 def fetchTicket(id):
-    # fetch ticket data for a ticket with ID: id
+    # Set the request parameters
     url = 'https://zccsenior.zendesk.com/api/v2/tickets/' + str(id) + '.json'
     
     # Do the HTTP get request
     start = time.time()
     response = requests.get(url, auth=(user, pwd))
     end = time.time()
-    print("\nAPI response time %.2f" % ((end-start)*1000) + " microseconds.\n")
-
+    print("\nAPI response time %.2f" % ((end-start)*1000) + " microseconds.\n") 
+    
     if response.status_code != 200:
         print('Status:', response.status_code, 'Problem with the request. Exiting....')
         exit()
@@ -72,13 +45,30 @@ def fetchTicket(id):
     data = response.json()
     return data
 
+def fetchQuarter(page):
+    print("\nPage " + str(page) + " of " + str(math.ceil(count['value']/25)))
+    
+    pageNum = ((page%4)-1) * 25
+    url = 'https://zccsenior.zendesk.com/api/v2/tickets.json?page=' + str(math.ceil(page/4))
+
+    # Do the HTTP get request
+    start = time.time()
+    response = requests.get(url, auth=(user, pwd))
+    end = time.time()
+    print("\nAPI response time %.2f" % ((end-start)*1000) + " microseconds.\n") 
+    
+    if response.status_code != 200:
+        print('Status:', response.status_code, 'Problem with the request. Exiting....')
+        exit()
+    
+    data = response.json()['tickets'][pageNum:count['value'] if count['value'] <= pageNum+25 else pageNum+25] 
+    return data
+
 def displayTicket(ticket):
-    # display details of a ticket
     print("\nTicket: " + str(ticket['id']) + "\n\nSubject: " + str(ticket['subject']))
     print("\nDescription: " + str(ticket['description']) + "\n\nCreated on: " + ticket['created_at'])
 
 def displayTickets(tickets):
-    # display tickets on a list
     if(len(tickets) <= 25):
         for ticket in tickets:
             print("Ticket ID: " + str(ticket['id']) + " subject '" + ticket['subject'] + "' created on " + ticket['created_at'])
@@ -88,86 +78,95 @@ def menu():
     while(1):
         print("\nPlease enter choice from options below\n1. Display all tickets.")
         choice = int(input("2. Display a ticket \n3. Quit\n\n"))
-
-        # display all tickets /start with page 1/
         if(choice == 1):
             print("\nThis user has " + str(count['value']) + " tickets currently registered. Refreshed at: " + str(count['refreshed_at']))
-            displayTickets(fetchTickets(1)['tickets'])
             currentPage = 1
-            lastPage = len(urls)
-            print("\nPage " + str(currentPage) + " of " + str(lastPage))
+            displayTickets(fetchQuarter(1))
+            lastPage = math.ceil(count['value']/25)
+            print("\nPage " + str(currentPage) + " of " + str(math.ceil(count['value']/25)))
             
             while(1):
                 print("\nPlease choose from the following options")
             #first page choices
                 if(currentPage == 1):
-                    choice = int(input("1. Next Page.\n2. Jump to Page.\n3. Display a ticket.\n4. Return to Main Menu\n\n"))
+                    choice = int(input("1. Next Page.\n2. Jump to Page.\n3. Display a ticket.\n4. Quit\n\n"))
                     if (choice == 1):
                         currentPage += 1
-                        displayTickets(fetchTickets(currentPage)['tickets'])
-                        print("\nPage " + str(currentPage) + " of " + str(lastPage))
+                        displayTickets(fetchQuarter(currentPage))
+                        print("\nPage " + str(currentPage) + " of " + str(math.ceil(count['value']/25)))
+                    
                     elif(choice == 2):
                         pageNum = int(input("Enter page number: "))
                         if(pageNum >= 1 and pageNum <= lastPage):
                             currentPage = pageNum
-                            displayTickets(fetchTickets(currentPage)['tickets'])
+                            displayTickets(fetchQuarter(currentPage))
                             print("\nPage " + str(currentPage) + " of " + str(lastPage))
+                        
                         else: print("\nInvalid Page Number.")
+        
                     elif(choice == 3):
                         id = int(input("Please enter ticket ID of ticket: "))
                         if(id > count['value'] and id < 1):
                             print("Invalid id number.")
                         else: displayTicket(fetchTicket(id)['ticket'])
                     elif(choice == 4):
-                        break 
+                        print("\nExiting. . . Goodbye:)")
+                        exit()
                     else: print("Invalid choice.")
             #last page choices            
                 elif(currentPage == lastPage):
-                    choice = int(input("1. Previous Page.\n2. Jump to Page.\n3. Display a ticket.\n4. Return to Main Menu\n\n"))
+                    choice = int(input("1. Previous Page.\n2. Jump to Page.\n3. Display a ticket.\n4. Quit\n\n"))
                     if (choice == 1):
                         currentPage -= 1
-                        displayTickets(fetchTickets(currentPage)['tickets'])
-                        print("\nPage " + str(currentPage) + " of " + str(lastPage))
+                        displayTickets(fetchQuarter(currentPage))
+                        print("\nPage " + str(currentPage) + " of " + str(math.ceil(count['value']/25)))
+                    
                     elif(choice == 2):
                         pageNum = int(input("Enter page number: "))
                         if(pageNum >= 1 and pageNum <= lastPage):
                             currentPage = pageNum
-                            displayTickets(fetchTickets(currentPage)['tickets'])
+                            displayTickets(fetchQuarter(currentPage))
                             print("\nPage " + str(currentPage) + " of " + str(lastPage))
-                        else: print("\nInvalid page number.")
+                        
+                        else: print("\nInvalid Page Number.")
+        
                     elif(choice == 3):
                         id = int(input("Please enter ticket ID of ticket: "))
                         if(id > count['value'] and id < 1):
                             print("Invalid id number.")
                         else: displayTicket(fetchTicket(id)['ticket'])
                     elif(choice == 4):
-                        break
+                        print("\nExiting. . . Goodbye:)")
+                        exit()
                     else: print("Invalid choice.") 
             #mid page choices
                 else: 
-                    choice = int(input("1. Previous page.\n2. Next Page.\n3. Jump to Page.\n4. Display a ticket.\n5. Return to Main Menu\n\n"))
+                    choice = int(input("1. Previous page.\n2. Next Page.\n3. Jump to Page.\n4. Display a ticket.\n5. Quit\n\n"))
                     if (choice == 1):
                         currentPage -= 1
-                        displayTickets(fetchTickets(currentPage)['tickets'])
+                        displayTickets(fetchQuarter(currentPage))
                         print("\nPage " + str(currentPage) + " of " + str(lastPage))
                     elif (choice == 2):
                         currentPage += 1
-                        displayTickets(fetchTickets(currentPage)['tickets'])
+                        displayTickets(fetchQuarter(currentPage))
                         print("\nPage " + str(currentPage) + " of " + str(lastPage))
                     elif (choice == 3):
                         pageNum = int(input("Enter page number: "))
                         if(pageNum >= 1 and pageNum <= lastPage):
                             currentPage = pageNum
-                            displayTickets(fetchTickets(currentPage)['tickets'])
+                            displayTickets(fetchQuarter(currentPage))
                             print("\nPage " + str(currentPage) + " of " + str(lastPage))
-                        else: print("\nInvalid page number.")
+                        
+                        else: print("\nInvalid Page Number.")
+                    
                     elif (choice == 4):
                         id = int(input("Please enter ticket ID of ticket: "))
                         if(id > count['value'] and id < 1):
                             print("Invalid id number.")
                         else: displayTicket(fetchTicket(id)['ticket'])
                     elif(choice == 5):
-                        break
+                        print("\nExiting. . . Goodbye:)")
+                        exit()
                     else: print("Invalid choice.")
 
         elif(choice == 2):
@@ -176,10 +175,9 @@ def menu():
                 print("Invalid id number. Returning to main menu.")
             else: displayTicket(fetchTicket(id)['ticket'])
         elif(choice == 3):
-            print("\nExiting...\n\n")
+            print("\nExiting. . . Goodbye:)")
             exit()
         else: print("\nChoice not available!", end=" ")
-
 
 init()
 menu()
